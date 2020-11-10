7aae05ec916bb0249ca9614d9118d3c00866feb7
Martin Diaz <martinleodiaz@gmail.com>
2020-11-10 18:47:33 -0300

    solucion base mvc angular web api

---
+++ ProyectoClipMoney2020/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+using ProyectoClipMoney2020.App_Start;
+using System.Web.Optimization;
+
+namespace ProyectoClipMoney2020
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Código que se ejecuta al iniciar la aplicación
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
