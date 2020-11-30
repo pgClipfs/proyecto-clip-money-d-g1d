fbb7af7e86534784c53250e418dcf2cd0a02ca62
Melani Gisel Crespo <melani.crespo@globant.com>
2020-11-30 12:31:16 -0300

    agregamos modal quienes soms en home drawer

--- app/src/app/Componentes/home/home.component.ts
+++ app/src/app/Componentes/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/Servicios/authentication.service';
+import { ModalQuienesSomosService } from '../../Servicios/modal-quienes-somos.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { AuthenticationService } from 'src/app/Servicios/authentication.service'
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService, private modalQuienesSomosService: ModalQuienesSomosService) { }
 
   ngOnInit(): void {
   }
@@ -23,6 +24,10 @@ export class HomeComponent implements OnInit {
   
   closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+  }
+
+  llamarModal() {
+    this.modalQuienesSomosService.Alert('MoneyClip es una billetera virtual. Accede a tu dinero rápido, fácil y en cualquier parte. Desarrollado por: Nicolas Alvarez, Jimena Bustos Paulich, Melani Crespo, Martin Diaz, Maximiliano Iglesias del Castillo, Matias LLorens, Joel Ocampo, Melania Peralta Flores, Tomas Pozzo * Programa Clip 2020 - Grupo 1D', 'Conoce a nuestro Equipo!', 'i');
   }
 
 }
