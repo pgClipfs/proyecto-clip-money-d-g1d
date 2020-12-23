845d3b69cae6e253b89c43501eabf217ef41ad08
Melani Gisel Crespo <melani.crespo@globant.com>
2020-12-23 15:35:59 -0300

    empezamos formulario para cambio de datos

--- app/src/app/Componentes/mi-perfil/mi-perfil.component.ts
+++ app/src/app/Componentes/mi-perfil/mi-perfil.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from '../../Servicios/cliente.service';
+import { Cliente } from '../../Modelos/Cliente';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-mi-perfil',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiPerfilComponent implements OnInit {
 
-  constructor() { }
+  FormMiPerfil: FormGroup;
+  Submited: false;
 
-  ngOnInit(): void {
+
+  constructor(public formBuilder: FormBuilder, private clienteService: ClienteService) 
+  {
+
   }
+
+  ngOnInit()
+  {
+    this.FormMiPerfil = this.formBuilder.group(
+      {
+        IdCliente: [0],
+        Nombre: [''],
+        Apellido: [''],
+        FechaNacimiento: [''],
+        TipoDocumento: [''],
+        NroDocumento: [''],
+        /*     FotoFrenteDocumento: [''],
+            FotoDorsoDocumento: [''], */
+        Email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}')]],
+        Telefono: ['', [Validators.required, Validators.pattern('[0-9]{20}')]],
+        Domicilio: ['',[Validators.required, Validators.maxLength(120)]],
+        Nacionalidad: [''],
+   /*      PassEncriptada: [''], */
+        Usuario: ['']
+        /*     SituacionCrediticia: [''],
+            Cuentas: [''], */
+      });
+
+      this.CargarUsuario();
+
+  }
+
+  CargarUsuario(){
+
+   /*  this.clienteService.getById() */
+  }
+  
 
 }
