b6df42693ae2b386bd93f891f546b304c4721368
Martin Diaz <martinleodiaz@gmail.com>
2020-12-28 17:35:27 -0300

    servicio y modelo recuperar password

---
+++ app/src/app/Modelos/RecuperarPass.ts
@@ -0,0 +1,9 @@
+export class RecuperarPass {
+    idCliente: number;
+    email: string;
+    nombre: string;
+    apellido: string;
+    password: string;
+    cod: string;
+    fechaRecup: Date;
+  }
