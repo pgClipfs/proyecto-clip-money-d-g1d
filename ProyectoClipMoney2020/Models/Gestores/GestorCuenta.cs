e21acd46c6cd3246f7d5bb878fb0108da413a5db
Martin Diaz <martinleodiaz@gmail.com>
2021-01-05 21:47:07 -0300

    actualizacion de back cuenta

--- ProyectoClipMoney2020/Models/Gestores/GestorCuenta.cs
+++ ProyectoClipMoney2020/Models/Gestores/GestorCuenta.cs
@@ -9,6 +9,53 @@ namespace ProyectoClipMoney2020.Models
 {
     public class GestorCuenta
     {
+        public bool ValidarCuentaExistente(long idCliente)
+        {
+            string strConn = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
+            bool result = true;
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                conn.Open();
+
+                SqlCommand comm = new SqlCommand("buscarCuentasActivas", conn);
+                comm.CommandType = System.Data.CommandType.StoredProcedure;
+                comm.Parameters.Add(new SqlParameter("@idCliente", idCliente));
+
+
+                SqlDataReader reader = comm.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    result = false;
+                }
+
+            }
+            return result;
+        }
+
+
+        public int CrearCuenta(long idCliente)
+        {
+            int id = 0;
+            string StrConn = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
+
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+
+                SqlCommand comm = conn.CreateCommand();
+                comm.CommandText = "crearCuentaPesos";
+                comm.CommandType = System.Data.CommandType.StoredProcedure;
+                //comm.Parameters.Add(new SqlParameter("@idCliente", cliente.idCliente));
+                comm.Parameters.Add(new SqlParameter("@idCliente", idCliente));               
+
+                id = Convert.ToInt32(comm.ExecuteScalar());
+
+            }
+            return id;
+        }
+
         public List<Cuenta> ObtenerCuentas()
         {
             var cuentas = new List<Cuenta>();
@@ -18,7 +65,7 @@ namespace ProyectoClipMoney2020.Models
             {
                 conn.Open();
                 SqlCommand comm = conn.CreateCommand();
-                comm.CommandText = "obtener_cuenta";
+                comm.CommandText = "obtener_cuentas";
                 comm.CommandType = System.Data.CommandType.StoredProcedure;
 
                 SqlDataReader dr = comm.ExecuteReader();
@@ -49,6 +96,73 @@ namespace ProyectoClipMoney2020.Models
             return cuentas;
         }
 
+        public int ActualizarCuenta(int id,Cuenta cuenta)
+        {
+            int boo = 0;            
+            string StrConn = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();            
+            
+                using (SqlConnection conn = new SqlConnection(StrConn))
+                {
+                    conn.Open();
+
+                    SqlCommand comm = conn.CreateCommand();
+                    comm.CommandText = "actualizarAlias";
+                    comm.CommandType = System.Data.CommandType.StoredProcedure;
+                    //comm.Parameters.Add(new SqlParameter("@idCliente", cliente.idCliente));
+                    comm.Parameters.Add(new SqlParameter("@idCliente", id));
+                    comm.Parameters.Add(new SqlParameter("@alias", cuenta.alias));                  
+
+                    boo = Convert.ToInt32(comm.ExecuteScalar());
+
+                }
+            
+
+            return boo;
+           
+        }
+
+        public Cuenta ObtenerCuentaPesosPorIdCliente(long idCliente)
+        {
+            var cuenta = new Cuenta();
+            string StrConn = ConfigurationManager.ConnectionStrings["BDLocal"].ToString();
+
+            using (SqlConnection conn = new SqlConnection(StrConn))
+            {
+                conn.Open();
+
+                SqlCommand comm = new SqlCommand("obtener_cuentaXid", conn);
+                comm.CommandType = System.Data.CommandType.StoredProcedure;
+                comm.Parameters.Add(new SqlParameter("@idCliente", idCliente));
+
+                SqlDataReader dr = comm.ExecuteReader();
+                if (dr.Read())
+                {
+                    cuenta.cvu = dr.GetString(0);
+                    cuenta.alias = dr.GetString(1).Trim();
+                    cuenta.saldo = dr.GetDecimal(2);
+                    if (!dr.IsDBNull(3))
+                        cuenta.observacion = dr.GetString(3)?.Trim();
+                    var tipoCuenta = new TipoCuenta()
+                    {
+                        idTipoCuenta = dr.GetByte(4),
+                        nombreTipoCuenta = dr.GetString(5)
+                    };
+                    var estadoCuenta = new EstadoCuenta()
+                    {
+                        idEstadoCuenta = dr.GetByte(6),
+                        nombreEstadoCuenta = dr.GetString(7)
+                    };
+                    cuenta.tipoCuenta = tipoCuenta;
+                    cuenta.estadoCuenta = estadoCuenta;
+
+                }
+
+                dr.Close();
+            }
+
+            return cuenta;
+
+        }
 
         public Cuenta ObtenerCuentaPorCvu(string cvu)
         {
@@ -69,8 +183,8 @@ namespace ProyectoClipMoney2020.Models
                     cuenta.cvu = dr.GetString(0);
                     cuenta.alias = dr.GetString(1).Trim();
                     cuenta.saldo = dr.GetDecimal(2);
-                    if (!dr.IsDBNull(3))
-                        cuenta.observacion = dr.GetString(3)?.Trim();
+                    if(!dr.IsDBNull(3))
+                    cuenta.observacion = dr.GetString(3)?.Trim();
                     var tipoCuenta = new TipoCuenta()
                     {
                         idTipoCuenta = dr.GetByte(4),
@@ -135,4 +249,3 @@ namespace ProyectoClipMoney2020.Models
         }
     }
 }
-
